1c958da5fac04a69379fe65d35b42c0b1442979e
Justin Davis <justindaniel92@gmail.com>
2020-08-02 13:10:59 -0500

    Day 0: First day of challenge, unable to complete during hour

--- log.md
+++ log.md
@@ -7,3 +7,12 @@
 **Thoughts:** This has already got me pushing my comfort zone. I am uncomfortable posting on Twitter, but I will try and go ahead with it. 
 
 **Link to work:** Maybe I'll start here tomorrow... https://www.freecodecamp.org/learn
+
+
+### Day 0: August 2, 2020
+
+**Today's Progress**: Started working on a freeCodeCamp Algorithm Challenge (Find the Symmetric Difference).
+
+**Thoughts:** I'm somewhat frustrated that I thought I could get through this challenge somewhat easily. But for some reason there a few elements to this that have caught me by surprise, and I've been unable to finish this challenge within an hour limit. 
+
+**Link to work:** [Algorithms - Symmetric Difference](projects/1_algorithms_symmetric_difference/main.js)
---
+++ projects/1_algorithms_symmetric_difference/main.js
@@ -0,0 +1,59 @@
+function retrieve_workable_arguments(argument_collection){
+  // Get arguments situated where its easier to deal with dynamic passing.
+  var arguments_passed = [];
+  Object.keys(argument_collection).forEach((passed_array)=>{
+    arguments_passed.push(argument_collection[passed_array]);
+  })
+  return arguments_passed
+}
+
+function remove_any_duplicate(argument_collection){
+  var all_arrays = [];
+
+  for (var set_argument of argument_collection){
+    var new_set = [];
+    for (var value of set_argument){
+      if (!new_set.includes(value)){
+        new_set.push(parseInt(value));
+      }
+    }
+    all_arrays.push(new_set);
+  }
+
+  return all_arrays;
+}
+
+function count_number_of_occurances(argument_collection){
+  var value_reference_count = {};
+  // Count the number of occurances
+  for (var passed_array of argument_collection){
+    for (var value of passed_array){
+      value_reference_count[value] = value_reference_count[value] + 1 || 1;
+    }
+  }
+  return value_reference_count;
+}
+
+function build_symmetric_set(value_reference_count){
+  var symmetric_set = [];
+  // Go back through, and find values with only count as 1. 
+  Object.keys(value_reference_count).forEach((key)=>{
+    if (value_reference_count[key] == 1){
+      symmetric_set.push(parseInt(key));
+    }
+  }); 
+
+  return symmetric_set;
+}
+
+function sym() {
+  
+  var arguments_passed = retrieve_workable_arguments(arguments);
+  arguments_passed = remove_any_duplicate(arguments_passed);
+  var value_reference_count = count_number_of_occurances(arguments_passed);
+  
+  return build_symmetric_set(value_reference_count);
+}
+
+var value = sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]); // Failing Test...
+console.log(value);
