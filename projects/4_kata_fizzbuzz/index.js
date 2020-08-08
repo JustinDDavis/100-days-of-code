ed254a783a89d0e55f0b312e7e39bcbaca41b1cd
Justin Davis <justindaniel92@gmail.com>
2020-08-08 14:18:10 -0500

    Day 6: My freeform FizzBuzz solution.

--- projects/4_kata_fizzbuzz/index.js
+++ projects/4_kata_fizzbuzz/index.js
@@ -2,12 +2,15 @@ function fizzBuzz(numberOfLines) {
   const fizzBuzzOutput = (index) => {
     if (index === (numberOfLines + 1)) return;
 
-    if (index % 3 == 0) {
+    if ((index % 3 === 0) && (index % 5 === 0)) {
+      console.log('FizzBuzz');
+    } else if (index % 3 === 0) {
       console.log("Fizz");
-    }else {
+    } else if (index % 5 === 0) {
+      console.log("Buzz");
+    } else {
       console.log(index);
     }
-    
     fizzBuzzOutput(index + 1);
   };
 
